380691998ca16679a668fa09b2ab82b4062d9773
kyy008 <keyuyangunis@126.com>
2025-10-17 19:13:38 +0800

    init: 多模块项目init

---
+++ HCMU-server/src/main/java/org/hcmu/HcmuServerApplication.java
@@ -0,0 +1,13 @@
+package org.hcmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmuServerApplication.class, args);
+	}
+
+}
