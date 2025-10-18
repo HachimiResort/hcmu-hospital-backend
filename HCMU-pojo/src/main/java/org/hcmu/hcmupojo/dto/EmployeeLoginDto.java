b3313308585e9c379a7667b47d4fcad1467dda18
kyy008 <keyuyangunis@126.com>
2025-10-18 18:08:13 +0800

    fix: 多模块依赖问题修复

---
+++ HCMU-pojo/src/main/java/org/hcmu/hcmupojo/dto/EmployeeLoginDto.java
@@ -0,0 +1,12 @@
+package org.hcmu.hcmupojo.dto;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+public class EmployeeLoginDto {
+
+}
