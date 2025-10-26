ddcf2731022adc6f48908f7f26aa86d52f756312
HFDLYS <845388925@qq.com>
2025-10-26 17:20:43 +0800

    feat: 为什么不配置cors

--- HCMU-server/src/main/java/org/hcmu/hcmuserver/mapper/user/UserRoleMapper.java
+++ HCMU-server/src/main/java/org/hcmu/hcmuserver/mapper/user/UserRoleMapper.java
@@ -9,8 +9,8 @@ import org.hcmu.hcmupojo.entity.relation.UserRole;
  *  SysUserRoleMapper 接口
  * </p>
  *
- * @author Zhichao Li
- * @since 2024-03-22
+ * @author Jiasheng Wang
+ * @since 2024-03-20
  */
 @Mapper
 public interface UserRoleMapper extends MPJBaseMapper<UserRole> {
